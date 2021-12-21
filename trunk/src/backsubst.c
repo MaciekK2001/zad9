a38a71fa333e9593e9f6ab266303006aa3e33e2a
Karol S <karol.sekscinski@wp.pl>
2021-12-21 20:56:19 +0100

    wstecz podstawienie

--- trunk/src/backsubst.c
+++ trunk/src/backsubst.c
@@ -1,22 +1,17 @@
 #include "backsubst.h"
 /**
- * Zwraca 0 - wsteczne podstawienie zakonczone sukcesem
- * Zwraca 1 - błąd dzielenia przez 0 (element na diagonali = 0)
- * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
- */
+ *  * Zwraca 0 - wsteczne podstawienie zakonczone sukcesem
+ *   * Zwraca 1 - błąd dzielenia przez 0 (element na diagonali = 0)
+ *    * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
+ *     */
 int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
-
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
-
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
-
-				return 0;
+        x->data[mat->r-1][0] = b->data[b->r-1][0] / mat->data[mat->r-1][mat->r-1];
+		        for ( int wiersz = mat->r-2; wiersz >= 0; wiersz--)     {
+		                x->data[i][0] = b->data[i][0];
+	                for( int kolumna = wiersz+1; kolumna < mat->r -1; kolumna++)
+			       	x->data[wiersz][0] = x->data[wiersz][0] - mat->data[wiersz][kolumna] * x->data[kolumna][0];
+	                x->data[wiersz][0] = x->data[wiersz][0] / mat->data[wiersz][wiersz];
+	                }
+			                return 0;
 }
-
 
