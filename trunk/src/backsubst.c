05b6d86255caf0843322d55fdbb4acb8b2b040f3
MaciekK2001 <maciekk2001@DESKTOP-9TRUN9I.localdomain>
2021-12-21 21:43:06 +0100

    poprawiony błąd w backsubst.c związany z odwołaniem się w tablicy do niezadeklarowanej zmiennej

--- trunk/src/backsubst.c
+++ trunk/src/backsubst.c
@@ -7,7 +7,7 @@
 int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
         x->data[mat->r-1][0] = b->data[b->r-1][0] / mat->data[mat->r-1][mat->r-1];
 		        for ( int wiersz = mat->r-2; wiersz >= 0; wiersz--)     {
-		                x->data[i][0] = b->data[i][0];
+		                x->data[wiersz][0] = b->data[wiersz][0];
 	                for( int kolumna = wiersz+1; kolumna < mat->r -1; kolumna++)
 			       	x->data[wiersz][0] = x->data[wiersz][0] - mat->data[wiersz][kolumna] * x->data[kolumna][0];
 	                x->data[wiersz][0] = x->data[wiersz][0] / mat->data[wiersz][wiersz];
